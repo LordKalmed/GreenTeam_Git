e9795f4c9f4ce967780d20d8f4466d9d898886f1
Thinesh <thinesh55@hotmail.co.uk>
2021-10-15 14:12:23 +0100

    Including 2 runs

--- anguslog.py
+++ anguslog.py
@@ -3,7 +3,7 @@ def run_command():
     run_away_now = print("Hurry UP and RUN!!!")
     return run_away
     return run_away_now
-
+    return run_away_now_now
 run_command()
 
 #Changed the function code so it will run
