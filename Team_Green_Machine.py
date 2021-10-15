c8edc3eae571c6a7330d222c33a807d7ce6c2b13
myounis789 <mohammedyounis789@gmail.com>
2021-10-15 14:25:46 +0100

    Fixing the conflict

--- Team_Green_Machine.py
+++ Team_Green_Machine.py
@@ -1,9 +1,5 @@
 def Green_Team():
-    names = "Chester, Thinesh, Younis, Angus, Imposter"
+    names = "Chester, Thinesh, Younis, Angus"
     return names
 
-<<<<<<< HEAD
-print("Green Team is the best!")
-=======
-print("This is a conflict!")
->>>>>>> 702578c0e00ac03c68bdcfea85f029d7de9d258a
+print("This is a creating a conflict!")
